8bf4fc2f40e83de78a4f32c9cca1b0ed6d8d67f8
syed_saleemuddin <saleemsyed815@gmail.com>
2022-06-04 18:17:43 +0530

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+let display=document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'))
+
+buttons.map(button =>{
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText='';
+                break;
+
+            case '←':
+                display.innerText=display.innerText.slice(0,-1);
+                break;
+                
+            case '=':
+                display.innerText= eval(display.innerText);
+                break;
+
+            default:
+                display.innerText += e.target.innerText;
+                breaak;
+        }
+    });
+});
